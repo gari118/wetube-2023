7440b93d94594244b250fdc1b545c3f2e70b451f
gari19 <gari118@gmail.com>
2023-08-31 22:35:07 +0900

    Upload - Post Video

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,18 +1,64 @@
-const fakeUser = {
-    username: "Gari",
-    loggedIn: true
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    },
+    {
+        title: "Second Video",
+        rating: 3,
+        comments: 23,
+        createAt: "2 minutes ago",
+        views: 59,
+        id: 2
+    },
+    {
+        title: "Third Video",
+        rating: 4,
+        comments: 22,
+        createAt: "2 minutes ago",
+        views: 100,
+        id: 3
+    }
+];
+
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
 };
 
-export const trending = (req, res) =>
-    res.render("home", { pageTitle: "Home", fakeUser });
-export const see = (req, res) => {
-    return res.render("watch", { pageTitle: "Watch" });
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const edit = (req, res) => {
-    return res.render("edit", { pageTitle: "Edit" });
+
+export const postUpload = (req, res) => {
+    const { title } = req.body;
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createAt: "2 minutes ago",
+        views: 0,
+        id: videos.length + 1
+    };
+    videos.push(newVideo);
+    return res.redirect("/");
 };
-export const search = (req, res) => res.send("Search Video");
-export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
-};
-export const uploadVideo = (req, res) => res.send("Upload Video");
