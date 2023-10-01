ec4f3116cd2d38be2f7b66c28be04f3a0d2f3261
gari19 <gari118@gmail.com>
2023-10-02 04:48:29 +0900

    last

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userControllers";
+import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get("logout", logout);
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
-userRouter.get("/:id", see);
+userRouter.get("/remove", remove);
+userRouter.get(":id", see);
 
 export default userRouter;
